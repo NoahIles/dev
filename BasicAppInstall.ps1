97df2ad48c74f1b2c112ff69a6a2ce63bbd1e6d1
NoahIles <31051719+NoahIles@users.noreply.github.com>
2017-08-15 17:33:18 -0700

    Add files via upload

---
+++ BasicAppInstall.ps1
@@ -0,0 +1,33 @@
+#AppList
+    $applist = ('googlechrome','chromium','notepadplusplus','firefox','sharex','windirstat','7zip','VisualStudioCode','steam','vlc','qbittorrent','spotify','discord','putty','malwarebytes','battle.net')
+    $chocolateyinstall = $true
+#Functions
+    function Get-Applist {
+        param($applist)
+            $Applications = @()
+            $AllResponse = Read-Host -Prompt "Type (A) to install all apps $($applist)"
+            foreach($app in $applist){
+                if($AllResponse -like 'A'){
+                    $Applications = $applist
+                    break
+                }
+                else{
+                    $response = Read-Host -Prompt "Would you like to install $($App)"
+                    if(($response -like 'Y') -or ($response -like 'Ye') -or ($response -like 'Yes')){
+                        $Applications += $app
+                    }
+                }
+            }
+        return $Applications
+    }
+    function Install-Apps{
+        param($Applications)
+            choco install $Application -y -r 
+        return
+    }
+#Commands
+    if($chocolateyinstall -eq $true){
+        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) #Installs Chocolatey 
+    }
+    $Applications = Get-Applist -Applist $Applist
+    Install-Apps -Applications $Applications
