5d0f4b6434782358e301655d3f1d151fbfc4ecf9
NoahIles <31051719+NoahIles@users.noreply.github.com>
2017-08-16 11:33:17 -0700

    Update Ninite2.ps1

--- Ninite2.ps1
+++ Ninite2.ps1
@@ -1,33 +1,33 @@
 #AppList
-    $applist = ('googlechrome','chromium','notepadplusplus','firefox','sharex','windirstat','7zip','VisualStudioCode','steam','vlc','qbittorrent','spotify','discord','putty','malwarebytes','battle.net','keepassx','dropbox','git')
-    $chocolateyinstall = $true
+$applist = ('googlechrome','chromium','notepadplusplus','firefox','sharex','windirstat','7zip','VisualStudioCode','steam','vlc','qbittorrent','spotify','discord','putty','malwarebytes','battle.net','keepassx','dropbox','git')
+$chocolateyinstall = $true
 #Functions
-    function Get-Applist {
-        param($applist)
-            $Applications = @()
-            $AllResponse = Read-Host -Prompt "Type (A) to install all apps $($applist)"
-            foreach($app in $applist){
-                if($AllResponse -like 'A'){
-                    $Applications = $applist
-                    break
-                }
-                else{
-                    $response = Read-Host -Prompt "Would you like to install $($App)"
-                    if(($response -like 'Y') -or ($response -like 'Ye') -or ($response -like 'Yes')){
-                        $Applications += $app
-                    }
+function Get-Applist {
+    param($applist)
+        $Applications = @()
+        $AllResponse = Read-Host -Prompt "Type (A) to install all apps $($applist -join "`n")"
+        foreach($app in $applist){
+            if($AllResponse -match 'A|all'){
+                $Applications = $applist
+                break
+            }
+            else{
+                $response = Read-Host -Prompt "Would you like to install $($App)"
+                if(($response -like 'Y') -or ($response -like 'Ye') -or ($response -like 'Yes')){
+                    $Applications += $app
                 }
             }
-        return $Applications
-    }
-    function Install-Apps{
-        param($Applications)
-            choco install $Applications -y -r 
-        return
-    }
+        }
+    return $Applications
+}
+function Install-Apps{
+    param($Applications)
+        choco install $Applications -y -r 
+    return
+}
 #Commands
-    if($chocolateyinstall -eq $true){
-        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) #Installs Chocolatey 
-    }
-    $Applications = Get-Applist -Applist $Applist
-    Install-Apps -Applications $Applications
+if($chocolateyinstall -eq $true){
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) #Installs Chocolatey 
+}
+$Applications = Get-Applist -Applist $Applist
+Install-Apps -Applications $Applications
