85b234b25172e6b2e5d7515de449633970b2bea1
NoahIles <noahiles@gmail.com>
2020-11-22 12:36:37 -0800

    added admin limitation

--- Ninite2.ps1
+++ Ninite2.ps1
@@ -1,3 +1,4 @@
+#Requires -RunAsAdministrator
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $applist = ('7zip','audacity','chromium','cinebench','cpu-z','discord','disk2vhd','dropbox','ds4windows','git','googlechrome','googledrive','hashtab','jre8','keepassx','logitechgaming','malwarebytes','nomacs','obs-studio','paint.net','prime95','qbittorrent','rufus','sharex','steam','VisualStudioCode','vlc','windirstat')
 #Functions
