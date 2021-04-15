3102dee5b8064fa4c015fee0e367a52c8033415e
NoahIles <31051719+NoahIles@users.noreply.github.com>
2021-04-15 04:53:48 -0700

    Update Ninite2.ps1
    Updated applists, made app genres, wsl2 initial.

--- Ninite2.ps1
+++ Ninite2.ps1
@@ -2,7 +2,26 @@
 #Installs Chocolatey 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 #List of apps to install
-$applist = ('7zip','audacity','chromium','cinebench','cpu-z','discord','disk2vhd','dropbox','ds4windows','git','googlechrome','googledrive','hashtab','jre8','keepassx','logitechgaming','malwarebytes','nomacs','obs-studio','paint.net','prime95','qbittorrent','rufus','sharex','steam','VisualStudioCode','vlc','windirstat')
-$min_applist = ('Vivaldi','7zip','audacity','cinebench','cpu-z','discord','disk2vhd','dropbox','git','googlechrome','hashtab','jre8','keepassx','logitechgaming','malwarebytes','nomacs','obs-studio','qbittorrent','rufus','steam','VisualStudioCode','vlc','windirstat')
+$telemetry_stuff = ('blackbird', 'disable-nvidia-telemetry')
+$Bundles = ('adobereader','office365business')
+$Dev_stuff = ('jdk8','jre8','vcredist140', 'git', 'VisualStudioCode','mingw')
+$Utilities = ('nircmd', 'hashtab', '7zip', 'disk2vhd', 'windirstat', 'rufus')
+$benchMarks = ('cpu-z','cinebench','prime95')
+$apps = ('Vivaldi','audacity','discord','google-drive-file-stream','dropbox','googlechrome','keepassx','logitechgaming','nomacs','obs-studio','qbittorrent','steam','vlc')
+$install_List = $telemetry_stuff + $Bundles + $Dev_stuff + $Utilities + $apps
 #Install Command 
-choco install $applist -y -r 
+choco install $install_List -y -r 
+
+#WSL 2 stuff 
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+wsl --set-default-version 2
+choco install wsl2 -params "/retry:true"
+choco install wsl-kalilinux
+
+# TODO:  and make interactive  
+# TODO: add git user configuration :: user.name user.email 
+# TODO: add WSL commands, oh-my-zsh
+# TODO: add VScode extensions // settings 
+# TODO: automate .ssh / .zshrc /
