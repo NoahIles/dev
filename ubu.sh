ffe592246b79251fb17a4c84feff4c1c3bcad927
Noah <noah@BigIsland.localdomain>
2021-04-23 22:45:04 -0700

    added some stuff

--- ubu.sh
+++ ubu.sh
@@ -10,7 +10,16 @@ sudo apt install ssh
 sudo systemctl enable --now ssh
 
 # Misc Installation tools 
-sudo apt install net-tools curl htop ncdu
+sudo apt install net-tools curl htop ncdu zsh git vim
+
+#Oh-My-ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
+/bin/cp -rf .zshrc $HOME/.zshrc
+/bin/cp -rf .vimrc $HOME/.vimrc
+
 
 # Zerotier VPN Stuff
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
@@ -20,5 +29,9 @@ sudo zerotier-cli join 1c33c1ced03bb70c
 # RDP/VNC stuff
 sudo apt install xfce4 xfce4-goodies
 
-# TODO: Add MC Server Stuff  
+# TODO: Add MC Server Stuff
+sudo apt install screen openjdk-11-jre-headless default-jdk -y 
+sudo ufw allow 25565
+
+
 # TODO: Add Xrdp Stuff 
