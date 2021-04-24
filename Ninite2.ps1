4980c0c62152ab19429367bbb2c3d8f2ab1f887c
NoahIles <31051719+NoahIles@users.noreply.github.com>
2021-04-23 19:59:35 -0700

    Added 32 bit c++ stuff unxutils
    unxutils made g++  and make work within windows

--- Ninite2.ps1
+++ Ninite2.ps1
@@ -24,7 +24,7 @@ else {
 #* App Genres
 $telemetry_stuff = ('blackbird', 'disable-nvidia-telemetry')
 $bundles = ('adobereader','office365business')
-$dev_stuff = ('jdk8','jre8','vcredist140', 'git', 'VisualStudioCode','mingw')
+$dev_stuff = ('jdk8','jre8','vcredist140', 'git', 'VisualStudioCode','mingw', 'unxutils')
 $utilities = ('nircmd', 'hashtab', '7zip', 'disk2vhd', 'windirstat', 'rufus')
 $benchMarks = ('cpu-z','cinebench','prime95')
 $apps = ('Vivaldi','audacity','discord','google-drive-file-stream','dropbox','googlechrome','keepassx','logitechgaming','nomacs','obs-studio','qbittorrent','steam','vlc')
