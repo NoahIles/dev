2a4bdb2e61978ba38e9c72dce455a69970c67f32
Noah <Noahiles@gmail.com>
2021-05-21 00:57:50 -0700

    updated install script

--- install.ps1
+++ install.ps1
@@ -1,7 +1,8 @@
 #Requires -RunAsAdministrator
+#Run with `Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.github.com/NoahIles/quickstart/Windows/install.ps1'))`
 $install_location = "$HOME/Documents"
 Set-Location $install_location
 Invoke-WebRequest https://github.com/NoahIles/quickstart/archive/Windows.zip -OutFile 'quickstart.zip'
-Expand-Archive quickstart
+Expand-Archive "./quickstart.zip"
 $Install_App_Script = (Get-ChildItem $install_location/quickstart-Windows -Filter "Ninite2.ps1" ).FullName 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression $Install_App_Script
