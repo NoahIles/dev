0c475e935900c84ba18956bc43f953aea9a5bff1
Noah <Noahiles@gmail.com>
2021-05-20 23:52:36 -0700

    added more Posh-theme stuff

--- Ninite2.ps1
+++ Ninite2.ps1
@@ -1,10 +1,10 @@
 #Requires -RunAsAdministrator
 #User input Function
-function Prompt-User(){
-    param($prompt_string)
+function QuestionUser() {
+    param([string]$prompt_string)
     $response = Read-Host -Prompt "$prompt_string"
     $response = $response.toLower()
-    if(($response -like 'y') -or ($response -like 'ye') -or ($response -like 'yes')){
+    if (($response -like 'y') -or ($response -like 'ye') -or ($response -like 'yes')) {
         return $true
     }
     else {
@@ -13,12 +13,30 @@ function Prompt-User(){
 }
 # Oh-My-posh Installation 
 Install-Module oh-my-posh -Scope CurrentUser
-$SET_THEME Set-PoshPrompt -Theme cinnamon
-Add-Content $PROFILE $SET_THEME #powershell configuration file  
+#If local theme exists move/replace in themes folder
+if (QuestionUser -prompt_string "Would you like to Copy Powershell Themes") {
+
+    if (test-path "cinnamon.omp.json" ) {
+        $path = (Get-ChildItem -Path "$HOME/Documents/WindowsPowerShell/Modules/oh-my-posh/" -Filter "cinnamon.omp.json" -Recurse).Directory.FullName
+        Copy-Item ".\cinnamon.omp.json" $path -Force
+        Write-Output "Copied Provided Cinnamon theme to the themes folder"
+    }
+    if (Test-Path "*.omp.*json" -and ^"cinnamon*") {
+        Write-Output "Extra Themes found Copying to theme folder"
+        Set-Location $PSScriptRoot
+        $items = (Get-Item "*.omp.json" | Where-Object Name -notlike "cinnamon*").FullName
+        if ($items.Length -gt 0) {
+            $items | ForEach-Object copy-item $_ $path -force
+
+        }
+    }
+    $SET_THEME = Set-PoshPrompt -Theme cinnamon
+    Add-Content $PROFILE $SET_THEME #powershell configuration file  
+}
+
 # Installs Chocolatey if needed
 $chocoInstalled = powershell choco -v
-if(-not($chocoInstalled))
-{
+if (-not($chocoInstalled)) {
     write-output "Seems Chocolatey is not installed, installing now"
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
@@ -27,40 +45,40 @@ else {
 }
 #* App Genres
 $telemetry_stuff = ('blackbird', 'disable-nvidia-telemetry') # I think both of these packeges are broken atm
-$bundles = ('adobereader','office365business')
-$dev_stuff = ('jdk8','jre8','vcredist140', 'git', 'VSCode','mingw')
-$utilities = ('nircmd', 'hashtab', '7zip', 'disk2vhd', 'windirstat', 'rufus', '7-taskbar-tweaker','autohotkey')
-$benchMarks = ('cpu-z','cinebench','prime95')
-$apps = ('Vivaldi','audacity','discord','google-drive-file-stream','dropbox','googlechrome','keepassx','logitechgaming','nomacs','obs-studio','qbittorrent','steam','vlc')
+$bundles = ('adobereader', 'office365business')
+$dev_stuff = ('jdk8', 'jre8', 'vcredist140', 'git', 'VisualStudioCode', 'mingw', 'unxutils')
+$utilities = ('nircmd', 'hashtab', '7zip', 'disk2vhd', 'windirstat', 'rufus', '7-taskbar-tweaker', 'autohotkey')
+$benchMarks = ('cpu-z', 'cinebench', 'prime95')
+$apps = ('Vivaldi', 'audacity', 'discord', 'google-drive-file-stream', 'dropbox', 'googlechrome', 'keepassx', 'logitechgaming', 'nomacs', 'obs-studio', 'qbittorrent', 'steam', 'vlc')
 $install_List = @()
 $genreTable = @{
-    Telemetry_Stuff = $telemetry_stuff # Disables some Telemetry
-    Bundles = $bundles 
+    Telemetry_Stuff   = $telemetry_stuff # Disables some Telemetry
+    Bundles           = $bundles 
     Development_Tools = $dev_stuff # Java, vscode/C++ tools
-    Utility_Programs = $utilities # MISC
-    Main_Apps = $apps
+    Utility_Programs  = $utilities # MISC
+    Main_Apps         = $apps
 }
 #* Prompts the user for which Genres they want to install
 foreach ($Genre in $genreTable.keys) {
-    $r = Prompt-User -prompt_string "Would you like to install $Genre : $($genreTable[$Genre])"
-    if($r){
+    $r = QuestionUser -prompt_string "Would you like to install $Genre : $($genreTable[$Genre])"
+    if ($r) {
         $install_List += ($genreTable[$Genre])
     }
 }
 #* Installs Selected Apps/Genres
-if(($install_List) -and (Prompt-User -prompt_string "Install_List created Begin installing packeges?")){
+if (($install_List) -and (QuestionUser -prompt_string "Install_List created Begin installing packeges?")) {
     #$len = $install_List.length() #! figure out Why $install_List is a string instead of an Array Object
     write-output "Preparing to Install packeges"
     choco install $install_List -y -r 
 }
 #* I know that setting-sync exists this might be a better solution but also would lead to bloated vscode
 #VSCODE extension stuff
-if(Prompt-User -prompt_string "Would you Like to Install base VSCODE Extensions"){
+if (QuestionUser -prompt_string "Would you Like to Install base VSCODE Extensions") {
     $extensions = "aaron-bond.better-comments,ahmadawais.shades-of-purple,donjayamanne.githistory,dracula-theme.theme-dracula,eamodio.gitlens,esbenp.prettier-vscode,Gruntfuggly.todo-tree,kevinkyang.auto-comment-blocks,maptz.regionfolder,ms-vscode-remote.vscode-remote-extensionpack,ms-vscode.cpptools"
-    $extensions | % {"code --install-extensions $_"}
+    $extensions | ForEach-Object { "code --install-extensions $_" }
 }
 #* WSL 2 stuff 
-if(Prompt-User -prompt_string "Would you Like to Install WSL2?"){
+if (QuestionUser -prompt_string "Would you Like to Install WSL2?") {
     write-output "Enabling Required Windows Features"
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
