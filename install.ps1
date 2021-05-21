4fe3de9277556c5afeace0ef50915acbb6d70812
Noah <Noahiles@gmail.com>
2021-05-21 01:11:32 -0700

    more updated install script

--- install.ps1
+++ install.ps1
@@ -3,6 +3,10 @@
 $install_location = "$HOME/Documents"
 Set-Location $install_location
 Invoke-WebRequest https://github.com/NoahIles/quickstart/archive/Windows.zip -OutFile 'quickstart.zip'
-Expand-Archive "./quickstart.zip"
+Expand-Archive "./quickstart.zip" ./
 $Install_App_Script = (Get-ChildItem $install_location/quickstart-Windows -Filter "Ninite2.ps1" ).FullName 
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression $Install_App_Script
+if($Install_App_Script){
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression $Install_App_Script
+}else {
+    Write-Output "Install Script Failed"
+}
