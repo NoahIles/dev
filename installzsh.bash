e38dbbaa6c5e5287afb87d479899f8b4ba10ef82
lightning <Noahiles@gmail.com>
2021-06-24 00:57:51 -0700

    updated install script

--- installzsh.bash
+++
@@ -1,4 +0,0 @@
-#apt install git powerline fonts-powerline -y
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
-echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
-chsh -s /bin/zsh
