9c5366bd1414bf6149e5291de72b820d8f7da063
NIslander <noahiles@gmail.com>
2021-09-28 15:47:26 -0700

    install-test script v1

---
+++ installEnv.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install --no-install-recommends -y apt-transport-https ca-certificates software-properties-common \
+    zsh git vim unzip wget curl htop tmux
+
+mkdir ~/development
+
+cd ~/development/
+curl -O https://github.com/NoahIles/quickstart/archive/devEnvs.zip
+unzip devEnvs.zip
+
+mkdir ~/development/.devcontainer/
+mv ~/development/quickstart-devEnvs/cppENV/* ~/development/.devcontainer/
+
+code ~/development/ 
+
+
