035284c73c7b9ececbc7b1f749d56bf15cfc7fc6
NoahIles <31051719+NoahIles@users.noreply.github.com>
2021-10-17 22:25:08 -0700

    brew -> brew cask

--- installEnv.sh
+++ installEnv.sh
@@ -8,10 +8,11 @@ dependencies=(
 )
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
-brew install ${dependencies[@]}
+brew cask install ${dependencies[@]}
 mkdir ~/development
 cd ~/development/
 /bin/bash -c "$(curl -O -L https://raw.githubusercontent.com/NoahIles/quickstart/devEnvs/cppEnv.tar)"
+code -n  .
 /bin/bash -c code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 tar xvf cppEnv.tar
-code -n  .
+rm -f ./cppEnv.tar
