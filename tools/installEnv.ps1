53690a37528023c362ac904e7b8875f04fd5a7bf
NIslander <noahiles@gmail.com>
2021-10-25 16:06:11 -0700

    updated Windows install script

--- tools/installEnv.ps1
+++ tools/installEnv.ps1
@@ -4,12 +4,34 @@
 #   - Docker or Docker Desktop (for the devcontainer to actually open this script can function without docker installed)
 
 # Run this Command with administrator powershell shell
-# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.github.com/NoahIles/quickstart/devEnvs/installEnv.ps1')) 
-mkdir ~/development
-cd ~/development/
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.github.com/NoahIles/quickstart/devEnvs/tools/installEnv.ps1')) 
+if(!$env:PROCESSOR_ARCHITECTURE.Contains("64")){
+    Write-Host "This script requires 64 bit architecture"
+    exit 1
+}
+elseif(!Test-Path "C:\Program Files\Microsoft VS Code\Code.exe"){
+    Write-Host "Microsoft VS Code not installed Please install And try again"
+    exit 1
+}
+elseif(!Test-Path "C:\Program Files\Docker\Docker\Docker.exe"){
+    Write-Host "Docker not installed Please install"
+    Write-Host "This script will complete without Docker but DevEnv wont launch without it."
+}
+else{
+    Write-Host "All requirements met"
+}
+if(!Test-Path ~/development){
+    mkdir -p ~/development
+}
+if(!Test-Path ~/.zsh_history){
+    "" | out-file ~/.zsh_history -Append
+}
+Set-Location ~/development/
+Write-Output "Installing devEnv deploy and debug config"
 curl https://github.com/NoahIles/quickstart/archive/devEnvs.zip -OutFile 'cppEnv.zip'
-"" | out-file ~/.zsh_history -Append
+Write-Output "Installing the code extension needed to open up the devEnvironment in vscode"
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-unzip xvf cppEnv.zip
+Expand-Archive ~/development/cppEnv.zip -DestinationPath ~/development/
 rm cppEnv.zip
+Write-Output "Cleaning Up..."
 code -n  .
