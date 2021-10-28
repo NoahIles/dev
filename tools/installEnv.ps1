6e3b670481b3d2eadccc717d72151ca328c5b0b4
NIslander <noahiles@gmail.com>
2021-10-27 22:05:47 -0700

    updated asking

--- tools/installEnv.ps1
+++ tools/installEnv.ps1
@@ -5,18 +5,23 @@
 
 # Run this Command with administrator powershell shell
 # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.github.com/NoahIles/quickstart/devEnvs/tools/installEnv.ps1')) 
+function askContinue {
+    Write-Host "Press 'y' to continue...Or Any Other Key to exit"
+    $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    if(!($key.Character -like 'y' -or $key.Character -like 'Y' -or $key.VirtualKeyCode -eq 13)) {
+        exit 1
+    }
+}
+
 if(!($env:PROCESSOR_ARCHITECTURE.Contains("64"))){
     Write-Host "This script requires 64 bit architecture"
     Write-Host "You might need a new computer/os"
-    Write-Host "Press any key to continue..."
-    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    exit 1
+    askContinue
+
 }
-elseif(!(Test-Path "C:\Program Files\Microsoft VS Code\Code.exe")){
+else if ((!(Test-Path "$HOME\AppData\Local\Programs\Microsoft Vs Code\Code.exe") -and !(Test-Path "C:\Program Files\Microsoft VS Code\Code.exe") )){
     Write-Host "Microsoft VS Code not installed Please install And try again"
-    Write-Host "Press any key to continue..."
-    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    exit 1
+    askContinue
 }
 elseif(!(Test-Path "C:\Program Files\Docker\Docker\Docker.exe")){
     Write-Host "Docker not installed Please install"
@@ -25,15 +30,15 @@ elseif(!(Test-Path "C:\Program Files\Docker\Docker\Docker.exe")){
 else{
     Write-Host "All requirements met"
 }
-if(!(Test-Path ~/development)){
-    mkdir -p ~/development
+if(!(Test-Path $HOME/development)){
+    mkdir $HOME/development
 }
-if(!(Test-Path ~/.zsh_history)){
-    "" | out-file ~/.zsh_history -Append
+if(!(Test-Path $HOME/.zsh_history)){
+    "" | out-file $HOME/.zsh_history -Append
 }
-Set-Location ~/development/
+Set-Location $HOME/development/ 
 Write-Output "Installing devEnv deploy and debug config"
-curl https://github.com/NoahIles/quickstart/archive/devEnvs.zip -OutFile 'cppEnv.zip'
+curl https://github.com/NoahIles/quickstart/archive/devEnvs.zip -OutFile 'cppEnv.zip' -SslProtocol 'Tls12' 
 Write-Output "Installing the code extension needed to open up the devEnvironment in vscode"
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 Expand-Archive ~/development/cppEnv.zip -DestinationPath ~/development/
