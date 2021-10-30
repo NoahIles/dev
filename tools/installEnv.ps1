9c82f70f4028d29c2091065aa76be21409d1f1af
NIslander <noahiles@gmail.com>
2021-10-29 19:02:06 -0700

    restructured code added comments

--- tools/installEnv.ps1
+++ tools/installEnv.ps1
@@ -5,6 +5,8 @@
 
 # Run this Command with administrator powershell shell
 # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.github.com/NoahIles/quickstart/devEnvs/tools/installEnv.ps1')) 
+
+# This is a Helper Function to promt the user if they want to continue with the installer or not
 function askContinue {
     Write-Host "Press 'y' or enter to continue...Or Any Other Key to exit"
     $key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
@@ -13,6 +15,29 @@ function askContinue {
     }
 }
 
+# This function creates the actual development environment for VSCode
+# Downloading the repository from github which includes a .devcontainer environment and a default debugging config
+function downloadDevEnv {
+    if(!(Test-Path $HOME/development)){
+        mkdir $HOME/development
+    }
+    if(!(Test-Path $HOME/.zsh_history)){
+        "" | out-file $HOME/.zsh_history -Append
+    }
+    Set-Location $HOME/development/ 
+    Write-Output "Installing devEnv deploy and debug config"
+    Invoke-WebRequest -Uri "https://api.github.com/repos/noahiles/quickstart/zipball/devEnvs" -OutFile 'cppEnv.zip'
+    Write-Output "Installing the code extension needed to open up the devEnvironment in vscode"
+    code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+    Expand-Archive ~/development/cppEnv.zip -DestinationPath ~/development/
+    rm cppEnv.zip
+    Write-Output "Cleaning Up..."
+    Write-Output "DONE"
+    askContinue
+    code -n  .
+}
+
+# This is another Helper function that will try to use windows winget to install dependencies
 function askInstall {
     Param(
         $app
@@ -26,7 +51,8 @@ function askInstall {
     }
 }
 
-
+# Check for dependencies before downloading the devEnvironment
+# Ask to install the dependencies if they are not installed
 if(!($env:PROCESSOR_ARCHITECTURE.Contains("64"))){
     Write-Host "This script requires 64 bit architecture"
     Write-Host "You might need a new computer/os"
@@ -45,20 +71,5 @@ elseif(!(Test-Path "C:\Program Files\Docker\Docker\Docker.exe")){
 else{
     Write-Host "All requirements met"
 }
-if(!(Test-Path $HOME/development)){
-    mkdir $HOME/development
-}
-if(!(Test-Path $HOME/.zsh_history)){
-    "" | out-file $HOME/.zsh_history -Append
-}
-Set-Location $HOME/development/ 
-Write-Output "Installing devEnv deploy and debug config"
-Invoke-WebRequest -Uri "https://api.github.com/repos/noahiles/quickstart/zipball/devEnvs" -OutFile 'cppEnv.zip'
-Write-Output "Installing the code extension needed to open up the devEnvironment in vscode"
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-Expand-Archive ~/development/cppEnv.zip -DestinationPath ~/development/
-rm cppEnv.zip
-Write-Output "Cleaning Up..."
-Write-Output "DONE"
-askContinue
-code -n  .
+
+downloadDevEnv
