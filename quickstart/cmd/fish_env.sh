9626d36c1c8880c40aba08afc041a9cfa6ca42bd
Noah Iles <noah@MacBook-Pro-de-Noah.local>
2026-01-15 18:54:37 -0800

    updated env path for fish

--- quickstart/cmd/fish_env.sh
+++ quickstart/cmd/fish_env.sh
@@ -2,7 +2,8 @@
 
 # 1. Get the script's directory, resolving any symlinks and ensuring an absolute path.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+PARENT_DIR="$(dirname $(dirname "$SCRIPT_DIR"))"
 
+#TODO: Make this idempotent
 cp -r $PARENT_DIR/env/fish/* $HOME/.config/fish/
 echo "source $HOME/.config/fish/essentials.fish" >> $HOME/.config/fish/config.fish
