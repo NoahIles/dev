d8e6b6703c764fb20f1f77625e04fd562c6a489e
Noah Iles <noah@MacBook-Pro-de-Noah.local>
2026-01-15 18:36:58 -0800

    updated OSX workflow and added initial brewfile for work

--- quickstart/osx.sh
+++ quickstart/osx.sh
@@ -2,18 +2,34 @@
 
 echo "Running osx quickstart"
 
+BACKUP_PATH="$HOME/BrewBackup/Brewfile"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# BACKUP Brew packages 
+
 if command -v brew >/dev/null 2>&1; then
   echo "brew is already installed, backing up..."
-  mkdir -p "$HOME/BrewBackup"
-  brew bundle dump --file="$HOME/BrewBackup/Brewfile" --force
+  mkdir -p "$(dirname $BACKUP_PATH)"
+  brew bundle dump --file="$BACKUP_PATH" --force
+  echo "Created file at $BACKUP_PATH" 
+  cat $BACKUP_PATH
+  # Ensure fzf is installed for next step
+  brew install fzf
 else
-  echo "installing brew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "You can install brew... using "
+  echo "/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""
+  exit
 fi
 
+BUNDLE=$(ls $SCRIPT_DIR/apps/ | fzf)
+brew bundle install --file=$SCRIPT_DIR/apps/$BUNDLE
 
+FISH_PATH="$(which fish)"
 
-# brew bundle install --file=$SCRIPT_PATH/apps/PersonalBrewfile
+if ! grep -q "^$FISH_PATH$" /etc/shells; then
+  echo "You may need this command to allow fish as shell"
+  echo 'echo "$(which fish)" | sudo tee -a /etc/shells'
+  echo "chsh -s $FISH_PATH"
+fi
 
-
-# brew bundle --file="$SCRIPT_PATH/apps/PersonalBrewfile"
--- quickstart/shared.sh
+++ quickstart/shared.sh
@@ -2,5 +2,10 @@
 # Copy Fish Config 
 echo "Running Shared scripts"
 
-# cp -r $PARENT_DIR/env/fish/* $HOME/.config/fish/
-# echo "source $HOME/.config/fish/essentials.fish" >> $HOME/.config/fish/config.fish
+# Fish 
+cp -r $PARENT_DIR/env/fish/* $HOME/.config/fish/
+echo "source $HOME/.config/fish/essentials.fish" >> $HOME/.config/fish/config.fish
+# Fisher
+#fisher
+
+
