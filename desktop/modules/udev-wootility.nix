430076af91c7fe65f1bcc9e37f8e235cb9435f36
Noah Iles <Noahiles@gmail.com>
2026-07-29 02:44:30 -0700

    udev: add Wootility device rules

---
+++ desktop/modules/udev-wootility.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "wooting-udev-rules";
+      destination = "/lib/udev/rules.d/70-wooting.rules";
+      text = ''
+        # Wooting One Legacy
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
+
+        # Wooting One update mode
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402", TAG+="uaccess"
+
+        # Wooting Two Legacy
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
+
+        # Wooting Two update mode
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403", TAG+="uaccess"
+
+        # Generic Wooting devices
+        SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", TAG+="uaccess"
+      '';
+    })
+  ];
+}
