0c5a5ef26028f4c1f165fa52215c0901d955c4e3
Noah Iles <git@nislands.xyz>
2026-08-21 17:39:20 -0700

    build(gaming): add unzip for protonup-qt

--- desktop/modules/gaming.nix
+++ desktop/modules/gaming.nix
@@ -5,6 +5,7 @@
 }: {
   environment.systemPackages = with pkgs; [
     protonup-qt
+    unzip # protonup-qt shells out to it for .zip releases
     pkgs-unstable.mangohud
     pkgs-unstable.vulkan-tools
     pkgs-unstable.osu-lazer-bin
